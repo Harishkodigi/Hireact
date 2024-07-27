173a5812eff6f2b436413776a1cf015e5fd83607
Harishkumar <harishkumar.kodigi@gmail.com>
2024-07-27 13:58:23 +0530

    Second commit about JSX,npm,node modules,package.json

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
-const parent = React.createElement(
-    "div",
-    { id: "" },
-    React.createElement("div", { id: "parent" }, [
-      React.createElement("h1", {}, "am h1 tag !"),
-      React.createElement("h2", {}, "am h2 tag !"),
-    ]),
-    React.createElement("div", { id: "children" }, [
-      React.createElement("h1", {}, "am h1 tag !"),
-      React.createElement("h2", {}, "am h2 tag !"),
-    ])
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Title = () => {
+  return <h1>React form React</h1>;
+};
+
+const elem = <h2>Hello harish</h2>;
+
+const HeadingComponent = () => {
+  return (
+    <div>
+      <h1>Am a heading Component My child component below</h1>
+      <Title />
+      {elem}
+    </div>
   );
-  
-  const heading = React.createElement("h1", { id: "heading" }, "Hello React !");
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(heading);
-  root.render(parent);
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent/>);
